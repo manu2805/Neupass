c7befdf989ef028f6376f355663c5babcb03001b
Manu Suresh <ns2@illinois.edu>
2015-11-07 10:38:27 -0600

    final 1.0v

---
+++ Neupass/Hash.swift
@@ -0,0 +1,9 @@
+//
+//  Hash.swift
+//  Neupass
+//
+//  Created by Manudeep N.s on 11/2/15.
+//  Copyright © 2015 Manudeep Suresh. All rights reserved.
+//
+
+import Foundation
---
+++ Neupass/Neupass-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Neupass-Bridging-Header.h
+//  Neupass
+//
+//  Created by Manudeep N.s on 11/2/15.
+//  Copyright © 2015 Manudeep Suresh. All rights reserved.
+//
+
+#ifndef Neupass_Bridging_Header_h
+#define Neupass_Bridging_Header_h
+
+
+#endif /* Neupass_Bridging_Header_h */
